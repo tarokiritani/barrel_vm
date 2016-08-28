f755f0ca700aa48d702e9f80a868b45264ad5847
Taro Kiritani <tonggutailang@gmail.com>
2016-08-28 11:41:04 +0000

    Simplify the read

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -18,8 +18,10 @@ class AnalysesController < ApplicationController
 	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 	})
 	s3 = Aws::S3::Client.new(region: 'us-west-2')
-	obj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/'+@analysis.file})
-	@vm = obj.body.read
+	obj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/'+@analysis.file.sub!("txt","vm")})
+	vm = obj.body.read.split(",")
+	sec = (1..vm.length).map{|x| (x/10000.0).to_s}
+	@vm = sec.zip(vm).map{|x,y| x+","+y}.join("\n")+"\n"
 	#obj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/whisker'+@analysis.file})
 	#@angle = obj.body.read 
 	@angle = "sec,angle\\n1,10\\n2,34\\n10,22\\n20,11\\n30,21\\n40,23\\n50,11\\n60,22\\n"
