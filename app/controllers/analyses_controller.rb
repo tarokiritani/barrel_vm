b25fd28ed14573c4f2761d67520d3f151122372c
Taro Kiritani <tonggutailang@gmail.com>
2016-08-29 10:31:42 +0000

    Read the contacts info for active touch

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -18,11 +18,18 @@ class AnalysesController < ApplicationController
 	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 	})
 	s3 = Aws::S3::Client.new(region: 'us-west-2')
-	xsgobj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/'+@analysis.file+'.txt'})
+	xsgobj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/' + @analysis.file + '.txt'})
 	@vm = xsgobj.body.read
-	whiskobj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/'+@analysis.file+'.whisk'})
+	whiskobj = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/' + @analysis.file + '.whisk'})
 	@angle = whiskobj.body.read
-	@ons = [10,20,30]
-	@offs = [15,25,35]
+	@ons = []
+	@offs = []
+	if @analysis.analysis_type == "active touch"
+	  contactobj = s3k.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'xsgs/' + @analysis.file + '.contact'})
+	  contacts = contactobj.body.read
+	  puts contacts
+	  @ons = [1,20,30]
+	  @offs = [15,25,35]
+	end
   end
 end
