2b1e0db427e9b34644fb1251e6ff306588480e00
Taro Kiritani <tonggutailang@gmail.com>
2017-01-22 12:15:13 +0000

    Connect to s3 for jupyter.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,5 +9,11 @@ class StaticPagesController < ApplicationController
   end
   
   def groupdata
+    Aws.config.update({
+	    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	})
+  	s3 = Aws::S3::Client.new(region: 'us-west-2')
+	  jupyter = s3.get_object({bucket:'elasticbeanstalk-us-west-2-680684803586', key:'electrophysiology.html'})
+	  @jupyter = jupyter.body.read
   end
 end
